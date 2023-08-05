0301fb09d3c45053f2f04a02e44a2c20b604fe9c
Nichkov Roman <nichkovroman@gmail.com>
2023-08-05 18:26:35 +0300

    feat: add section reviews

--- src/js/script.js
+++ src/js/script.js
@@ -6,24 +6,71 @@ customFunctions.handleAccordion()
 customFunctions.handleTabs()
 
 // vendors
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
+const swiperParentReviewVideo = new Swiper('.swiper--parents-video', {
     direction: 'horizontal',
-    loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1.2,
+            spaceBetween: 20,
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        576: {
+            slidesPerView: 2.5,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1025: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
     },
 
-    // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--reviews-parent-video',
+        prevEl: '.swiper-button-prev--reviews-parent-video',
+    },
+})
+const swiperParentReviewPhoto = new Swiper('.swiper--parents-photo', {
+    direction: 'horizontal',
+
+    slidesPerView: 5,
+    spaceBetween: 20,
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1.6,
+            spaceBetween: 20,
+        },
+        480: {
+            slidesPerView: 1.7,
+            spaceBetween: 20,
+        },
+        576: {
+            slidesPerView: 3.1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1025: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
     },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    navigation: {
+        nextEl: '.swiper-button-next--reviews-parent-photo',
+        prevEl: '.swiper-button-prev--reviews-parent-photo',
     },
 })
